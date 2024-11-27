c85ae895a567ee8b77fc22a517c2e4749f1e65c9
DevFaith <waitherafaith@outlook.com>
2024-11-27 14:27:12 +0300

    * fix(WorkingSpace.jsx): fix import statement formatting

--- client/src/Components/WorkingSpace.jsx
+++ client/src/Components/WorkingSpace.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import{ useState, useRef } from "react";
 
 const WorkingSpace = ({ question, response }) => {
   const [notes, setNotes] = useState("");
